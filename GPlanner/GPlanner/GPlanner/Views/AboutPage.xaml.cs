9e20a62ec0f0706e822fa6f13957ee583a05228e
Martin Danhier <martindanhier@hotmail.com>
2018-02-26 23:19:46 +0100

    Creating the base project. Added appcenter support too.

---
+++ GPlanner/GPlanner/GPlanner/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GPlanner.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
