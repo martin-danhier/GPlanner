181494d13a3ef5989dd598d5135f5569b8d6254a
Xaphok <xaphok@gmail.com>
2018-06-16 19:43:27 +0200

    sth

--- GPlanner/GPlanner/GPlanner/Views/ToDoListPage.xaml.cs
+++ GPlanner/GPlanner/GPlanner/Views/ToDoListPage.xaml.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GPlanner.Classes;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace GPlanner.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ToDoListPage : ContentPage
-	{
-		public ToDoListPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ToDoListPage : ContentPage
+    {
+        public ToDoListPage()
+        {
+            InitializeComponent();
+            List<TaskToDo> toDo = new List<TaskToDo>
+            {
+            new TaskToDo("Buy some eggs", "One pack or two",place:"shop")
+            };
+            ToDoList.ItemsSource = toDo;
+        }
+    }
 }
